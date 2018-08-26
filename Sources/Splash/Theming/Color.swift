aea685e8b44520b825785afa4c62b0d92120d75d
John Sundell <john@sundell.co>
2018-08-26 22:57:02 +0200

    Color: Exclude internal extension on Linux

--- Sources/Splash/Theming/Color.swift
+++ Sources/Splash/Theming/Color.swift
@@ -12,8 +12,10 @@ import Cocoa
 public typealias Color = NSColor
 #endif
 
+#if !os(Linux)
 internal extension Color {
     convenience init(red: CGFloat, green: CGFloat, blue: CGFloat) {
         self.init(red: red, green: green, blue: blue, alpha: 1)
     }
 }
+#endif
