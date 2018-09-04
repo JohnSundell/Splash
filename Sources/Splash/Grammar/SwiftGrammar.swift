32764b0720535ea2a7a80519c68f422ec51601cb
Marco Capano <marco24capano@icloud.com>
2018-09-04 22:26:29 +0200

    Add support for continue, fallthrough and repeat
    This PR introduces new keywords in SwiftGrammar

--- Sources/Splash/Grammar/SwiftGrammar.swift
+++ Sources/Splash/Grammar/SwiftGrammar.swift
@@ -47,7 +47,8 @@ private extension SwiftGrammar {
         "super", "self", "set", "true", "false", "nil",
         "override", "where", "_", "default", "break",
         "#selector", "required", "willSet", "didSet",
-        "lazy", "subscript", "defer"
+        "lazy", "subscript", "defer", "continue",
+        "fallthrough", "repeat"
     ]
 
     struct PreprocessingRule: SyntaxRule {
