e96fd0f0fcd94c7827f211cbf7d6f567da77e398
Matthaus Woolard <matthaus.woolard@gmail.com>
2020-06-04 20:29:40 +1200

    permit changin the Grammer used for the test case class

--- Sources/SplashTestKit/Core/SyntaxHighlighterTestCase.swift
+++ Sources/SplashTestKit/Core/SyntaxHighlighterTestCase.swift
@@ -11,12 +11,14 @@ import Splash
 /// Test case used as an abstract base class for all tests relating to
 /// syntax highlighting. For all such tests, the Swift grammar is used.
 open class SyntaxHighlighterTestCase: SplashTestCase {
+    public static var grammer: Grammar = SwiftGrammar()
+    
     private(set) public var highlighter: SyntaxHighlighter<OutputFormatMock>!
     private(set) public var builder: OutputBuilderMock!
 
-    public override func setUp() {
+    open override func setUp() {
         super.setUp()
         builder = OutputBuilderMock()
-        highlighter = SyntaxHighlighter(format: OutputFormatMock(builder: builder))
+        highlighter = SyntaxHighlighter(format: OutputFormatMock(builder: builder), grammar: Self.grammer)
     }
 }
